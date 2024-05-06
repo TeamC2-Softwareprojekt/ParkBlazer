d1f95e90c05276adae97f949316b9b53a84fdf98
MVBeats <real9ms@gmail.com>
2024-05-06 21:30:22 +0200

    added Maptiler

--- ParkBlazerApp/src/pages/Home.tsx
+++ ParkBlazerApp/src/pages/Home.tsx
@@ -1,59 +1,25 @@
-import MessageListItem from '../components/MessageListItem';
-import { useState } from 'react';
-import { Message, getMessages } from '../data/messages';
-import {
-  IonContent,
-  IonHeader,
-  IonList,
-  IonPage,
-  IonRefresher,
-  IonRefresherContent,
-  IonTitle,
-  IonToolbar,
-  useIonViewWillEnter
-} from '@ionic/react';
-import './Home.css';
+import React, { useEffect } from 'react';
+import * as maptilersdk from '@maptiler/sdk';
 
 const Home: React.FC = () => {
+  useEffect(() => {
+    // MapTiler-Karte initialisieren
+    maptilersdk.config.apiKey = 'K3LqtEaJcxyh4Nf6BEPT';
+    const map = new maptilersdk.Map({
+      container: 'map', // container's id or the HTML element to render the map
+      style: 'streets-v2',
+      center: [8.9189, 52.2900], // Minden, Deutschland (Längengrad, Breitengrad)
+      zoom: 14, // starting zoom
+    });
 
-  const [messages, setMessages] = useState<Message[]>([]);
-
-  useIonViewWillEnter(() => {
-    const msgs = getMessages();
-    setMessages(msgs);
-  });
-
-  const refresh = (e: CustomEvent) => {
-    setTimeout(() => {
-      e.detail.complete();
-    }, 3000);
-  };
+    return () => {
+      // Aufräumen beim Komponentenabbau, z.B. Karte zerstören
+      map.remove();
+    };
+  }, []);
 
   return (
-    <IonPage id="home-page">
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Inbox</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonRefresher slot="fixed" onIonRefresh={refresh}>
-          <IonRefresherContent></IonRefresherContent>
-        </IonRefresher>
-
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">
-              Inbox
-            </IonTitle>
-          </IonToolbar>
-        </IonHeader>
-
-        <IonList>
-          {messages.map(m => <MessageListItem key={m.id} message={m} />)}
-        </IonList>
-      </IonContent>
-    </IonPage>
+    <div id="map" style={{ width: '100vw', height: '100vh' }}></div>
   );
 };
 
