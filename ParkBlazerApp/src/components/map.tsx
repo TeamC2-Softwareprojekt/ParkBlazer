2af01f5ab227e049aa7ed9fd7e9a9c05b8c0a83e
MVBeats <real9ms@gmail.com>
2024-05-14 12:44:40 +0200

    added show all parkspots as  Marker on the map

--- ParkBlazerApp/src/components/map.tsx
+++ ParkBlazerApp/src/components/map.tsx
@@ -1,16 +1,17 @@
 import React, { useRef, useEffect, useState } from 'react';
 import * as maptilersdk from '@maptiler/sdk';
+import axios from 'axios';
 import "@maptiler/sdk/dist/maptiler-sdk.css";
 import './map.css';
-import Marker from './MarkerMenu'; // Import der Marker-Komponente
 import MarkerMenu from './MarkerMenu';
 
 export default function Map() {
   const mapContainer = useRef<HTMLDivElement>(null);
   const map = useRef<maptilersdk.Map | null>(null);
-  const tokyo = { lng: 139.753, lat: 35.6844 };
   const [zoom] = useState<number>(14);
-  maptilersdk.config.apiKey = 'K3LqtEaJcxyh4Nf6BEPT';
+  const [parkingSpots, setParkingSpots] = useState<any[]>([]); // Parkplätze speichern in einem Array
+
+  maptilersdk.config.apiKey = 'K3LqtEaJcxyh4Nf6BEPT'; // API Key setzen
 
   useEffect(() => {
     if (map.current) return;
@@ -21,7 +22,35 @@ export default function Map() {
       center: [8.9167, 52.2833],
       zoom: zoom
     });
-  }, [tokyo.lng, tokyo.lat, zoom]);
+    getParkingSpots();
+  }, [zoom]);
+
+  // Diese Funktion ruft die Parkplätze von der API ab und aktualisiert den State
+  const getParkingSpots = async () => {
+    try {
+      const response = await axios.get('https://server-y2mz.onrender.com/api/parkingspots');
+      setParkingSpots(response.data); // Parkplätze speichern
+      displayParkingSpotsOnMap(response.data); // Parkplätze auf der Karte anzeigen
+    } catch (error) {
+      console.error('Error getting all Parkingspots', error);
+    }
+  };
+
+  // Diese Funktion zeigt die Parkplätze auf der Karte an
+  const displayParkingSpotsOnMap = (parkingSpots: any[]) => {
+    if (!map.current) return; // prüft ob die Karte bereits geladen wurde
+
+    // Marker für jeden Parkplatz hinzufügen
+    parkingSpots.forEach((spot) => {
+      const { longitude, latitude, name, description } = spot;
+
+      // Marker zur Karte hinzufügen
+      new maptilersdk.Marker({ color: "#FF0000" })
+        .setLngLat([parseFloat(longitude), parseFloat(latitude)]) // Position setzen
+        .setPopup(new maptilersdk.Popup().setHTML(`<h3>${name}</h3><p>${description}</p>`)) // Popup hinzufügen
+        .addTo(map.current!);
+    });
+  };
 
   return (
     <div className="map-wrap">
