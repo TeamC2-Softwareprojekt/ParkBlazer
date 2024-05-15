d0b517522c003a96e3f95290e469b70f69117cab
Loeffler13 <oloeffler13@gmail.com>
2024-05-15 18:21:34 +0200

    fix

--- ParkBlazerApp/src/pages/Login.tsx
+++ ParkBlazerApp/src/pages/Login.tsx
@@ -4,11 +4,9 @@ import { IonInput, IonCol, IonGrid, IonRow, IonButton, IonAlert } from '@ionic/r
 import AuthService from '../AuthService';
 import './Login.css';
 
-
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [userValid, setUserValid] = useState<boolean>(true);
     const [error, setError] = useState('');
     const history = useHistory();  // useHistory Hook initialisieren
 
@@ -21,11 +19,9 @@ function Login() {
             setError(''); 
             await AuthService.login(email, password);
             console.log("Logged in!")
-            setUserValid(true)
             history.push('/home')
 
         } catch (error: any) {
-            setUserValid(false)
             setError(error.message);
         }
     };
@@ -39,22 +35,14 @@ function Login() {
                         placeholder="Email"
                         value={email}
                         onIonChange={(event) => setEmail(event.detail.value!)}
-<<<<<<< HEAD
-                        color={userValid ? "primary" : "danger"}
-=======
                         className="login-input"
->>>>>>> 5401c291ab0459c9d9ef4583715ceaf5d0f9d025
                     ></IonInput>
                     <IonInput
                         type="password"
                         placeholder="Password"
                         value={password}
                         onIonChange={(event) => setPassword(event.detail.value!)}
-<<<<<<< HEAD
-                        color={userValid ? "primary" : "danger"}
-=======
                         className="login-input"
->>>>>>> 5401c291ab0459c9d9ef4583715ceaf5d0f9d025
                     ></IonInput>
                     <IonButton onClick={handleLogin}>Login</IonButton>
                     {error && <IonAlert isOpen={!!error} message={error} buttons={['OK']} />}
