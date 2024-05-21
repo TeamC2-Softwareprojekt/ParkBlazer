e223a99fa6734bc867e4031c86660004833c23b8
MVBeats <real9ms@gmail.com>
2024-05-21 13:48:11 +0200

    added input of Attributes for the adding a Marker

--- ParkBlazerApp/src/components/MarkerMenu.tsx
+++ ParkBlazerApp/src/components/MarkerMenu.tsx
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
-import { IonContent, IonFab, IonFabButton, IonFabList, IonHeader, IonIcon, IonTitle, IonToolbar, IonModal, IonInput, IonButton, IonList, IonItem } from '@ionic/react';
+import { IonContent, IonFab, IonFabButton, IonFabList, IonHeader, IonIcon, IonTitle, IonToolbar, IonModal, IonInput, IonButton, IonList, IonItem, IonText } from '@ionic/react';
 import { chevronUpCircle, add } from 'ionicons/icons';
 
 function MarkerMenu() {
   const [showMenu, setShowMenu] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [showModalCoordinates, setShowModalCoordinates] = useState(false);
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [image, setImage] = useState('');
+  const [address, setAddress] = useState('');
+  const [errorLatitude, setErrorLatitude] = useState('');
+  const [errorLongitude, setErrorLongitude] = useState('');
+  const [errorTitle, setErrorTitle] = useState('');
+  const [errorAddress, setErrorAddress] = useState('');
+  const [errorDescription, setErrorDescription] = useState('');
+  const [errorImage, setErrorImage] = useState('');
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
@@ -17,16 +28,69 @@ function MarkerMenu() {
     setShowMenu(false); 
   };
 
+  const openModalCoordinates = () => {
+    setShowModalCoordinates(true);
+    setShowModal(false);
+  }
+
   const closeModal = () => {
     setShowModal(false);
   };
 
-  const handleSaveCoordinates = () => {
-    console.log('Latitude:', latitude);
-    console.log('Longitude:', longitude);
-    closeModal();
+  const closeModalCoordinates = () => {
+    setShowModalCoordinates(false);
   };
 
+  const handleSaveCoordinates = () => {
+    const lat = parseFloat(latitude);
+    const lng = parseFloat(longitude);
+    let valid = true;
+
+    // Validation of the Inputs
+    if (isNaN(lat) || lat < -90 || lat > 90) {
+      setErrorLatitude('Bitte geben Sie eine gültige Breite zwischen -90 und 90 ein.');
+      valid = false;
+    } else {
+      setErrorLatitude('');
+    }
+
+    if (isNaN(lng) || lng < -180 || lng > 180) {
+      setErrorLongitude('Bitte geben Sie eine gültige Länge zwischen -180 und 180 ein.');
+      valid = false;
+    } else {
+      setErrorLongitude('');
+    }
+
+    if (title.trim().length === 0) {
+      setErrorTitle('Bitte geben Sie einen Titel ein.');
+      valid = false;
+    } 
+    else {
+      setErrorTitle('');
+    }
+
+    if (address.trim().length === 0) {
+      setErrorAddress('Bitte geben Sie eine Adresse ein.');
+      valid = false;
+    } 
+    else {
+      setErrorAddress('');
+    }
+
+    if (description.trim().length === 0) {
+      setErrorDescription('Bitte geben Sie eine Beschreibung ein.');
+      valid = false;
+    } 
+    else {
+      setErrorDescription('');
+    }
+
+    if (valid) {
+      console.log('Latitude:', latitude);
+      console.log('Longitude:', longitude);
+      closeModalCoordinates();
+    }
+  };
   const handleUseCurrentLocation = () => {
     // Logik aktueller Standort
     console.log('Aktuellen Standort verwenden');
@@ -38,6 +102,7 @@ function MarkerMenu() {
     console.log('Auf der Karte auswählen');
     closeModal();
   };
+
 
   return (
     <>
@@ -60,7 +125,7 @@ function MarkerMenu() {
           </IonFabList>
         </IonFab>
 
-        {/* Modal zum Eingeben der Koordinaten */}
+        {/* Modal zum Erstellen eines Markers */}
         <IonModal isOpen={showModal} onDidDismiss={closeModal}>
           <IonContent>
             <IonList>
@@ -70,12 +135,103 @@ function MarkerMenu() {
               <IonItem button onClick={handleSelectLocationOnMap}>
                 Auf der Karte auswählen
               </IonItem>
-              <IonItem button onClick={handleSaveCoordinates}>
+              <IonItem button onClick={openModalCoordinates}>
                 Koordinaten eingeben
               </IonItem>
             </IonList>
           </IonContent>
         </IonModal>
+
+         {/* Modal zum Eingeben der Koordinaten */}
+         <IonModal isOpen={showModalCoordinates} onDidDismiss={closeModalCoordinates}>
+          <IonContent>
+            <IonList>
+              <IonItem>
+                <IonInput
+                  type="number"
+                  placeholder="Latitude"
+                  value={latitude}
+                  onIonChange={e => setLatitude(e.detail.value!)} 
+                />
+              </IonItem>
+              {errorLatitude && (
+                <IonItem>
+                  <IonText color="danger">{errorLatitude}</IonText>
+                </IonItem>
+              )}
+              <IonItem>
+                <IonInput
+                  type="number"
+                  placeholder="Longitude"
+                  value={longitude}
+                  onIonChange={e => setLongitude(e.detail.value!)}
+                />
+              </IonItem>
+              {errorLongitude && (
+                <IonItem>
+                  <IonText color="danger">{errorLongitude}</IonText>
+                </IonItem>
+              )}
+              <IonItem>
+                <IonInput
+                  type="text"
+                  placeholder="title"
+                  value={title}
+                  onIonChange={e => setTitle(e.detail.value!)} 
+                />
+              </IonItem>
+              {errorTitle && (
+                <IonItem>
+                <IonText color="danger">{errorTitle}</IonText>
+              </IonItem>
+              )}
+              <IonItem>
+                <IonInput
+                  type="text"
+                  placeholder="address"
+                  value={address}
+                  onIonChange={e => setAddress(e.detail.value!)} 
+                />
+              </IonItem>
+              {errorAddress && (
+                <IonItem>
+                <IonText color="danger">{errorAddress}</IonText>
+              </IonItem>
+              )}
+              <IonItem>
+                <IonInput
+                  type="text"
+                  placeholder="description"
+                  value={description}
+                  onIonChange={e => setDescription(e.detail.value!)} 
+                />
+              </IonItem>
+              {errorDescription && (
+                <IonItem>
+                <IonText color="danger">{errorDescription}</IonText>
+              </IonItem>
+              )}
+              <IonItem>
+                <IonInput
+                  type="url"
+                  placeholder="image"
+                  value={image}
+                  onIonChange={e => setImage(e.detail.value!)} 
+                />
+              </IonItem>
+              {errorImage && (
+                <IonItem>
+                <IonText color="danger">{errorImage}</IonText>
+              </IonItem>
+              )}
+              <IonButton onClick={handleSaveCoordinates}>Speichern</IonButton>
+            </IonList>
+          </IonContent>
+        </IonModal>
+
+        {/* Modal zum Erstellen eines Parkplatzes mit Auswahl auf der Karte  */}
+
+
       </IonContent>
     </>
   );
