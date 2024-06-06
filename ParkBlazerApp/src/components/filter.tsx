70359d03ec44e117e687ad70b0afd661b6d952ab
Bent Schöne <bent.schoene@fh-bielefeld.de>
2024-06-06 13:57:54 +0200

    Refactor Filter * Change DOM manipulation to ref hooks

--- ParkBlazerApp/src/components/filter.tsx
+++ ParkBlazerApp/src/components/filter.tsx
@@ -15,13 +15,26 @@ export interface FilterParams {
 }
 
 export default function Filter({onFilterApply}: {onFilterApply: any}) {
+    const segment = useRef<HTMLIonSegmentElement>(null);
+    const checkboxes = {
+        checkboxBike: useRef<HTMLIonCheckboxElement>(null),
+        checkboxCar: useRef<HTMLIonCheckboxElement>(null),
+        checkboxTruck: useRef<HTMLIonCheckboxElement>(null)
+    };
+    const inputs = {
+        inputPriceMin: useRef<HTMLIonInputElement>(null),
+        inputPriceMax: useRef<HTMLIonInputElement>(null),
+        inputSpaces: useRef<HTMLIonInputElement>(null),
+        inputCity: useRef<HTMLIonInputElement>(null),
+        inputRadius: useRef<HTMLIonInputElement>(null)
+    };
     const inputCity = {
         label: "city",
-        component: <IonInput id="filter-input-city" class="filter-input clear" label="Stadt" type="text" placeholder="Stadt" labelPlacement="stacked" fill="outline" clearOnEdit={true} inputMode="text"/>
+        component: <IonInput ref={inputs.inputCity} class="filter-input clear" label="Stadt" type="text" placeholder="Stadt" labelPlacement="stacked" fill="outline" clearOnEdit={true} inputMode="text"/>
     };
     const inputRadius = {
         label: "radius",
-        component: <IonInput id="filter-input-radius" class="filter-input clear" label="Radius" type="number" placeholder="0km" labelPlacement="stacked" fill="outline" clearOnEdit={true} inputMode="numeric" min={0}/>
+        component: <IonInput ref={inputs.inputRadius} class="filter-input clear" label="Radius" type="number" placeholder="0km" labelPlacement="stacked" fill="outline" clearOnEdit={true} inputMode="numeric" min={0}/>
     }
     const [showModal, setShowModal] = useState(false);
     const [searchMode, setSearchMode] = useState<any>(inputRadius);
@@ -31,39 +44,27 @@ export default function Filter({onFilterApply}: {onFilterApply: any}) {
     }
 
     function clearFilter() {
-        //TODO: use refs instead of DOM manipulation
-        const inputs = document.getElementsByClassName("clear") as HTMLCollectionOf<HTMLInputElement>;
-        const checkboxes = document.getElementsByClassName("filter-checkbox") as HTMLCollectionOf<HTMLInputElement>;
-
-        for (let input of inputs) {
-            input.value = "";
+        for (const key of Object.keys(inputs) as (keyof typeof inputs)[]) {
+            const input = inputs[key].current;
+            if (input) input.value = "";
         }
 
-        for (let checkbox of checkboxes) {
-            checkbox.checked = false;
+        for (const key of Object.keys(checkboxes) as (keyof typeof checkboxes)[]) {
+            const checkbox = checkboxes[key].current;
+            if (checkbox) checkbox.checked = false;
         }
     }
 
     function applyFilter() {
-        //TODO: use refs instead of DOM manipulation
-        const segment = document.getElementById("filter-segment") as HTMLIonSegmentElement;
-        const checkboxBike = document.getElementById("filter-checkbox-bike") as HTMLIonCheckboxElement;
-        const checkboxCar = document.getElementById("filter-checkbox-car") as HTMLIonCheckboxElement;
-        const checkboxTruck = document.getElementById("filter-checkbox-truck") as HTMLIonCheckboxElement;
-        const priceMin = document.getElementById("filter-input-price-min") as HTMLInputElement;
-        const priceMax = document.getElementById("filter-input-price-max") as HTMLInputElement;
-        const spaces = document.getElementById("filter-input-spaces") as HTMLInputElement;
-        const modeComponent = document.getElementById("filter-input-" + searchMode.label) as HTMLInputElement;
-
         let filterParams: FilterParams = {};
 
-        filterParams.category = segment.value;
-        filterParams.type_bike = checkboxBike.checked ? 1 : 0;
-        filterParams.type_car = checkboxCar.checked ? 1 : 0;
-        filterParams.type_truck = checkboxTruck.checked ? 1 : 0;
-        filterParams.mode = { mode: searchMode.label, value: modeComponent.value };
-        filterParams.price = { min: parseInt(priceMin.value), max: parseInt(priceMax.value) };
-        filterParams.minAvailableSpaces = parseInt(spaces.value);
+        filterParams.category = segment.current?.value;
+        filterParams.type_bike = checkboxes.checkboxBike.current?.checked ? 1 : 0;
+        filterParams.type_car = checkboxes.checkboxCar.current?.checked ? 1 : 0;
+        filterParams.type_truck = checkboxes.checkboxTruck.current?.checked ? 1 : 0;
+        filterParams.mode = { mode: searchMode.label, value: (inputs.inputCity.current?.value || inputs.inputRadius.current?.value) as string};
+        filterParams.price = { min: parseInt(inputs.inputPriceMin.current?.value as string), max: parseInt(inputs.inputPriceMax.current?.value as string) };
+        filterParams.minAvailableSpaces = parseInt(inputs.inputSpaces.current?.value as string);
 
         onFilterApply(filterParams);
     }
@@ -81,7 +82,7 @@ export default function Filter({onFilterApply}: {onFilterApply: any}) {
                                 <IonIcon icon={closeSharp}/>
                             </IonButton>
                     </IonHeader>
-                    <IonSegment id="filter-segment" value={'all'}>
+                    <IonSegment id="filter-segment" ref={segment} value={'all'}>
                         <IonSegmentButton value={"all"}>
                             <IonLabel>Alle</IonLabel>
                         </IonSegmentButton>
@@ -94,16 +95,16 @@ export default function Filter({onFilterApply}: {onFilterApply: any}) {
                     </IonSegment>
                     <div id="filter-input-container">
                         <div id="filter-checkbox-container">
-                            <IonCheckbox id="filter-checkbox-bike" className="filter-checkbox" labelPlacement="stacked">Fahrrad</IonCheckbox>
-                            <IonCheckbox id="filter-checkbox-car" className="filter-checkbox" labelPlacement="stacked">PKW</IonCheckbox>
-                            <IonCheckbox id="filter-checkbox-truck" className="filter-checkbox" labelPlacement="stacked">LKW</IonCheckbox>
+                            <IonCheckbox ref={checkboxes.checkboxBike} className="filter-checkbox" labelPlacement="stacked">Fahrrad</IonCheckbox>
+                            <IonCheckbox ref={checkboxes.checkboxCar} className="filter-checkbox" labelPlacement="stacked">PKW</IonCheckbox>
+                            <IonCheckbox ref={checkboxes.checkboxTruck} className="filter-checkbox" labelPlacement="stacked">LKW</IonCheckbox>
                         </div>
                         <div id="filter-price-container">
-                            <IonInput id="filter-input-price-min" class="filter-input clear" label="Minimum" type="number" placeholder="0€" labelPlacement="stacked" fill="outline" clearOnEdit={true} inputMode="numeric" min={0}/>
+                            <IonInput ref={inputs.inputPriceMin} class="filter-input clear" label="Minimum" type="number" placeholder="0€" labelPlacement="stacked" fill="outline" clearOnEdit={true} inputMode="numeric" min={0}/>
                             <IonLabel>-</IonLabel>
-                            <IonInput id="filter-input-price-max" class="filter-input clear" label="Maximum" type="number" placeholder="0€" labelPlacement="stacked" fill="outline" clearOnEdit={true} inputMode="numeric" min={0}/>
+                            <IonInput ref={inputs.inputPriceMax} class="filter-input clear" label="Maximum" type="number" placeholder="0€" labelPlacement="stacked" fill="outline" clearOnEdit={true} inputMode="numeric" min={0}/>
                         </div>
-                        <IonInput id="filter-input-spaces" class="filter-input clear" label="Anzahl an Parkplätzen" type="number" placeholder="0" labelPlacement="stacked" fill="outline" clearOnEdit={true} inputMode="numeric" min={0}/>
+                        <IonInput ref={inputs.inputSpaces} class="filter-input clear" label="Anzahl an Parkplätzen" type="number" placeholder="0" labelPlacement="stacked" fill="outline" clearOnEdit={true} inputMode="numeric" min={0}/>
                         <div id="filter-mode">
                             <IonSelect id="filter-select-mode" class="filter-input" label="Filtern nach" placeholder={searchMode.component.props.label} interface="popover" fill="outline" labelPlacement="stacked" onIonChange={(event) => switchSearchMode(event)}>
                               <IonSelectOption value="radius">Radius</IonSelectOption>
