dddcb91936cc0df40eb7e898206a148036a10328
HauerichT <timo.haverich@fh-bielefeld.de>
2024-06-19 17:10:04 +0200

    UPDATE modal for parkingspot details.

--- ParkBlazerApp/src/components/map.tsx
+++ ParkBlazerApp/src/components/map.tsx
@@ -8,18 +8,22 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 import "@maptiler/sdk/dist/maptiler-sdk.css";
 import './map.css';
 import MarkerMenu from './MarkerMenu';
-import { initParkingSpaces, parkingspaces } from '../data/parkingSpaces';
+import { initParkingSpaces, parkingSpace, parkingspaces } from '../data/parkingSpaces';
 import { getUserLocation } from '../data/userLocation';
+import { IonModal, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, IonText, IonIcon, IonCard, IonCardHeader, IonCardSubtitle } from '@ionic/react';
+import { checkmark, close, informationCircle } from 'ionicons/icons';
 
 let map: React.MutableRefObject<maptilersdk.Map | null>;
 
-export default function Map({onUpdateList, onLocationMarkerUpdate}: any) {
+export default function Map({ onUpdateList, onLocationMarkerUpdate }: any) {
   const mapContainer = useRef<HTMLDivElement>(null);
   map = useRef<maptilersdk.Map | null>(null);
   const [zoom] = useState<number>(14);
   const [mapController, setMapController] = useState<any>();
   const markUserLocationInterval = useRef<NodeJS.Timeout>();
   const locationMarker = useRef<maptilersdk.Marker>();
+  const [selectedSpot, setSelectedSpot] = useState<parkingSpace | null>(null);
+  const [showModal, setShowModal] = useState(false);
 
   maptilersdk.config.apiKey = 'K3LqtEaJcxyh4Nf6BEPT';
 
@@ -36,8 +40,8 @@ export default function Map({onUpdateList, onLocationMarkerUpdate}: any) {
     window.onload = () => map.current?.resize();
 
     const fetchParkingSpaces = async () => {
-        await initParkingSpaces();
-        displayParkingSpotsOnMap();
+      await initParkingSpaces();
+      displayParkingSpotsOnMap();
     }
 
     setMapController(createMapLibreGlMapController(map.current, maplibregl));
@@ -60,46 +64,34 @@ export default function Map({onUpdateList, onLocationMarkerUpdate}: any) {
     else startLocationMarkerUpdate();
     onUpdateList(event);
   }
-  
+
   const displayParkingSpotsOnMap = () => {
     if (!map.current) return;
 
     parkingspaces?.forEach((spot) => {
-      const getAvailabilityIcon = (available: number) => available === 1 ? '✔' : '✖';
-
-      const popupContent = `
-        <div style="color: black">
-          <h3 cursor="pointer" onclick="window.location.href='http://localhost:8100/parkingspot_details/${spot.parkingspot_id}'">${spot.name}</h3>
-          <p>${spot.description}</p>
-          <p>Verfügbare Plätze: ${spot.available_spaces}</p>
-          <p>${spot.image_url ? `<img src="${spot.image_url}" alt="Parkplatzbild" style="max-width: 100px;">` : ''}</p>
-          <p>Adresse: ${spot.street} ${spot.house_number}, ${spot.zip} ${spot.city}, ${spot.country}</p>
-          <p>Benutzer: ${spot.username}</p>
-          <p>Auto: ${getAvailabilityIcon(spot.type_car)}</p>
-          <p>Fahrrad: ${getAvailabilityIcon(spot.type_bike)}</p>
-          <p>Lastwagen: ${getAvailabilityIcon(spot.type_truck)}</p>
-        </div>
-      `;
-
-      new maptilersdk.Marker({ color: "#FF0000" })
+      const marker = new maptilersdk.Marker({ color: "#FF0000" })
         .setLngLat([spot.longitude, spot.latitude])
-        .setPopup(new maptilersdk.Popup().setHTML(popupContent))
         .addTo(map.current!);
+
+      marker.getElement().addEventListener('click', () => {
+        setSelectedSpot(spot);
+        setShowModal(true);
+      });
     });
   };
 
   const markUserLocation = () => {
-      const location = getUserLocation();      
+    const location = getUserLocation();
 
-      if (!location.latitude || !location.longitude || !map.current) return;
-      
-      if (locationMarker) locationMarker.current?.remove();
-      
-      locationMarker.current = new maptilersdk.Marker({ color: "#0000FF" });
-      locationMarker.current?.setLngLat([location.longitude, location.latitude]);
-      locationMarker.current?.setPopup(new maptilersdk.Popup().setHTML("<h3>Ihr Standort</h3>"));
-      locationMarker.current?.addTo(map.current);
-      onLocationMarkerUpdate();
+    if (!location.latitude || !location.longitude || !map.current) return;
+
+    if (locationMarker) locationMarker.current?.remove();
+
+    locationMarker.current = new maptilersdk.Marker({ color: "#0000FF" });
+    locationMarker.current?.setLngLat([location.longitude, location.latitude]);
+    locationMarker.current?.setPopup(new maptilersdk.Popup().setHTML("<h3>Ihr Standort</h3>"));
+    locationMarker.current?.addTo(map.current);
+    onLocationMarkerUpdate();
   };
 
   return (
@@ -108,11 +100,53 @@ export default function Map({onUpdateList, onLocationMarkerUpdate}: any) {
         <GeocodingControl apiKey={maptilersdk.config.apiKey} mapController={mapController} onPick={(e) => handleSearch(e)} />
       </div>
       <div ref={mapContainer} className="map" />
-      <div className="marker-container"> 
-      <MarkerMenu/>
+      <div className="marker-container">
+        <MarkerMenu />
       </div>
+
+      {selectedSpot && (
+        <IonModal className='modal_parkingspot_marker' isOpen={showModal} onDidDismiss={() => setShowModal(false)}>
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>{selectedSpot.name}</IonTitle>
+              <IonButton slot="end" onClick={() => window.open(`http://localhost:8100/parkingspot_details/${selectedSpot.parkingspot_id}`, '_self')}>
+                <IonIcon icon={informationCircle} />
+              </IonButton>
+              <IonButton id="btn-close" slot="end" onClick={() => setShowModal(false)}>
+                <IonIcon icon={close} />
+              </IonButton>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent>
+            <IonCard>
+              {selectedSpot.image_url && <img src={selectedSpot.image_url} alt="Parkplatzbild" style={{ maxWidth: '100%' }} />}
+            </IonCard>
+            <IonCard>
+              <IonCardHeader>
+                <IonCardSubtitle>{selectedSpot.description}</IonCardSubtitle>
+              </IonCardHeader>
+            </IonCard>
+            <IonCard>
+              <IonText><strong>Verfügbare Plätze:</strong> {selectedSpot.available_spaces}</IonText>
+            </IonCard>
+            <IonCard>
+              <IonText><strong>Adresse:</strong> {selectedSpot.street} {selectedSpot.house_number}, {selectedSpot.zip} {selectedSpot.city}, {selectedSpot.country}</IonText>
+            </IonCard>
+            <IonCard>
+              <IonText><strong>Angelegt von:</strong> {selectedSpot.username}</IonText>
+            </IonCard>
+            <IonCard>
+              <IonText><strong>Auto:</strong> <IonIcon icon={selectedSpot.type_car ? checkmark : close} /></IonText>
+              <br></br>
+              <IonText><strong>Fahrrad:</strong> <IonIcon icon={selectedSpot.type_bike ? checkmark : close} /></IonText>
+              <br></br>
+              <IonText><strong>Lastwagen:</strong> <IonIcon icon={selectedSpot.type_truck ? checkmark : close} /></IonText>
+            </IonCard>
+          </IonContent>
+        </IonModal>
+      )}
     </div>
   );
 }
 
-export {map};
+export { map };
