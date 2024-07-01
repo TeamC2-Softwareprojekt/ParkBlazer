b9704383603fda490e6d792706190458d9fb91e2
HauerichT <timo.haverich@fh-bielefeld.de>
2024-07-01 15:29:24 +0200

    Format the user_report page

--- ParkBlazerApp/src/pages/UserReports.tsx
+++ ParkBlazerApp/src/pages/UserReports.tsx
@@ -1,4 +1,4 @@
-import { IonPage, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonList, IonItem, IonThumbnail, IonLabel, IonText } from "@ionic/react";
+import { IonPage, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonList, IonItem, IonThumbnail, IonLabel, IonText, IonContent } from "@ionic/react";
 import Navbar from "../components/navbar";
 import { useState } from "react";
 import { useEffect } from "react";
@@ -35,29 +35,30 @@ const UserReports: React.FC = () => {
     return (
         <IonPage>
             <Navbar />
-            <IonGrid fixed className="userreports-grid">
-                <IonRow className="ion-justify-content-center ion-align-items-center full-height">
-                    <IonCol className="userreports-col" size="12" size-sm="12" size-md="12">
-                        <IonText color="primary" className="register-title">
-                            <h1 className="userreports-heading">Deine Meldungen</h1>
-                        </IonText>
-                        <IonCard>
-                            <IonCardContent>
-                                <IonList>
-                                    {reports && reports.map((report: any, index: any) => (
-                                        <IonItem key={index}>
-                                            <IonText>{report.report_type}</IonText>
-                                            <IonText>{report.description}</IonText>
-                                            <IonText>{report.status}</IonText>
-                                            <IonText>{report.report_date} {formatDistanceToNow(report.report_date, { addSuffix: true, locale: de })}</IonText>
-                                        </IonItem>
-                                    ))}
-                                </IonList>
-                            </IonCardContent>
-                        </IonCard>
-                    </IonCol>
-                </IonRow>
-            </IonGrid>
+            <IonContent>
+                <IonGrid fixed className="userreports-grid">
+                    <IonRow className="ion-justify-content-center ion-align-items-center full-height">
+                        <IonCol className="userreports-col" size="12" size-sm="12" size-md="12">
+                            <IonText color="primary" className="register-title">
+                                <h1 className="userreports-heading">Deine Meldungen</h1>
+                            </IonText>
+                            {reports && reports.map((report: any, index: any) => (
+                                <IonCard key={index}>
+                                    <IonCardContent>
+                                        <IonText><strong>Datum:</strong> {report.report_date} {formatDistanceToNow(report.report_date, { addSuffix: true, locale: de })}</IonText>
+                                        <br />
+                                        <IonText><strong>Grund:</strong> {report.report_type}</IonText>
+                                        <br />
+                                        <IonText><strong>Kommentar:</strong> {report.description}</IonText>
+                                        <br />
+                                        <IonText><strong>Status:</strong> {report.status}</IonText>
+                                    </IonCardContent>
+                                </IonCard>
+                            ))}
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+            </IonContent>
         </IonPage>
     );
 };
